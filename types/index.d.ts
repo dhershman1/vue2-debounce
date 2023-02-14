078c4e24a833556c0fc90abc5a6e3dd320e54331
dhershman1 <dustinh17@gmail.com>
2023-02-14 14:50:58 -0500

    Update: Tweak types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,3 @@
-/*
- * TODO 12/1/2019 - It would be nice if we could add types to the modifiers and expected callback value of the directive but this does not seem to be possible right now.
- *
- * Both of the types are marked as any without any generic support so not much we can do. This might change in the future when Vue is actually written in typescript as per the road map for Vue 3 https://github.com/vuejs/roadmap#3x
- * "value" type - https://github.com/vuejs/vue/blob/fd0eaf92948bb5a4882d538362091fb287d642e3/types/vnode.d.ts#L70
- * "modifiers" type - https://github.com/vuejs/vue/blob/fd0eaf92948bb5a4882d538362091fb287d642e3/types/options.d.ts#L184
- */
-
 export interface PluginConfig {
   lock?: boolean
   listenTo?: string | string[]
